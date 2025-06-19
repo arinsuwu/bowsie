4c07173a687a614141e77b421b1ee5c17353c6c1
Erik Rios <eselerik@outlook.com>
2025-06-19 14:15:37 -0600

    1.10: made %sub_<horz/vert>_pos() store distance to $0E

--- routines/sub_horz_pos.asm
+++ routines/sub_horz_pos.asm
@@ -1,10 +1,11 @@
-;===================================================
+;=======================================================
 ; sub_horz_pos: Check sprite's horizontal position
 ;               relative to player's
 ;
 ; Output:
+;  $0E contains the distance between player and sprite
 ;    Y is 0 if player is to the right, 1 otherwise
-;===================================================
+;=======================================================
 
     LDY $0DD6|!addr
     LDA $1F17|!addr,y
@@ -13,6 +14,7 @@
     SBC #$0008
     SEC
     SBC !ow_sprite_x_pos,x
+    STA $0E
     BPL $01
     INY
     RTL
